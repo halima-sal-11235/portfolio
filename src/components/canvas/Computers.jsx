401f9075949467f7b74c9c6e1b3547509e233033
Halima SalahOuelhadj <oh_salahouelhadj@esi.dz>
2026-09-11 09:59:40 +0100

    Refactor Computers component to use GLTF model and update lighting setup

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,8 +4,13 @@ import { OrbitControls,Preload,useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+  <mesh>
+    <hemisphereLight intensity={0.15} groundColor="black">
+
+    </hemisphereLight>
+  </mesh>
   )
 }
 
